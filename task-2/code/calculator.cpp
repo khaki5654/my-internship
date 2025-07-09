9a60151a5def63a2aac0613abc9963af936e4eda
khaki5654 <khaki5654@gmail.com>
2025-07-09 17:26:57 +0500

    portfolio

---
+++ task-2/code/calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b; char op;
+    cout<<"enter the operand";
+    cin>>op;
+
+    cout<<"enter first number = ";
+    cin>>a;
+    cout<<"enter second number = ";
+    cin>>b;
+switch (op)
+{
+case +:
+    cout<<a<<"+"<<b<<"="<<a+b;
+    break;
+    case -:
+      cout<<a<<"-"<<b<<"="<<a-b;
+      break;
+      case *:
+        cout<<a<<"*"<<b<<"="<<a*b;
+        break;
+        case /:
+          cout<<a<<"/"<<b<<"="<<a/b;
+          break;
+default:
+cout<<"invalid operator";
+    break;
+}
+
+return 0;
+
+}
+
+
+
